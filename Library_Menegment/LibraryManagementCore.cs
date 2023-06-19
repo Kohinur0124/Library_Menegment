186e831b5a81edc3e283472eccd2b326ff9f0d77
user <user@WIN-VG96EILPEET>
2023-06-19 18:54:01 +0500

    Change to  abstract

---
+++ Library_Menegment/LibraryManagementCore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Menegment
+{
+    public abstract class LibraryManagementCore
+    {
+        public LibraryStore Store { get; set; }
+
+        public LibraryManagementCore()
+        {
+            Store = new LibraryStore();
+        }
+        public LibraryManagementCore( List <Book> books , List <User> users)
+        {
+            Store = new LibraryStore(books,users);
+            
+        }
+        public void DisplayAllUsers() 
+        {
+        
+            foreach (User user in Store.Users)
+            {
+                Console.WriteLine(user.ToString());
+            }
+        }
+        public void DisplayAllBooks() 
+        {
+            foreach (Book book in Store.Books)
+            {
+                Console.WriteLine(book.ToString());
+            }
+        }
+
+    }
+}
